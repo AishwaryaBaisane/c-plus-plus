c552a2cbad3132831e0ed3f00dc295cd49c5c9ac
Aishwarya <149373597+AishwaryaBaisane@users.noreply.github.com>
2024-01-29 21:49:20 +0530

    Add files via upload

---
+++ emp&car/time.cpp
@@ -0,0 +1,27 @@
+//Q.3 Write a program to convert second into hh: mm: ss.
+#include<iostream>
+using namespace std;
+
+class Time
+{
+	public:
+	int second,hour,min;
+	void time()
+	{
+	  cout << "Enter second : ";
+	  cin >> second;
+	   hour = second / 3600;
+       min = second / 60; 
+       second = second % 60;
+
+	 cout <<"hh: mm: ss " << endl << hour << ':' << min << ':' << second ; 
+	}
+   
+	
+	
+};
+int main()
+{
+	Time s;
+	s.time();
+}
