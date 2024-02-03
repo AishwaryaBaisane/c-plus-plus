9b419155cb06aee29e793998ce975269583eaa2a
Aishwarya <149373597+AishwaryaBaisane@users.noreply.github.com>
2024-02-03 09:57:01 +0530

    Update 2.cpp

--- exam/2.cpp
+++ exam/2.cpp
@@ -1,42 +1,32 @@
-//Q.2 Write a Program to create a class to read time in seconds and convert it into time in (HH:MM: SS) format.
-//For example,
-//Input:
-//Enter seconds: 4520
-//
-//Output:
-//1:15:20
+//Q.3 Write a program to convert second into hh: mm: ss.
 #include<iostream>
 using namespace std;
 
-class time
+class Time
 {
 	public:
-    int h,m,s;
-    void input()
-    {
-       cout << "enter second :";
-	   cin >> s;	
-	}
-	void output()
-	{
-		h = s / 3600;
-		m = s % 60;
-		s = s % 60;
-		
-		cout << h  << ':'<< m << ':'<< s;
-	}
 
- 	
+	int s,hour,min;
+
+	void time()
+	{
+		
+	  cout << "Enter second : ";
+	  cin >> s;
+	  
+	   hour = s/3600;
+	   s=s-(hour*3600);
+	   min=s/60;
+	   s=s-(min*60);
+	   
+	 cout <<"hh: mm: ss " << endl << hour << ':' << min << ':' << s ; 
+	}
+   
+	
+	
 };
 int main()
 {
-	time m1;
-	m1.input();
-	m1.output();
-	
-		
-	
+	Time s1;
+	s1.time();
 }
-
-//inch /12
-//feet ++;
