e718e0bf31800209da952faeb344118f3e7fd85a
Aishwarya <149373597+AishwaryaBaisane@users.noreply.github.com>
2024-02-07 19:33:44 +0530

    Add files via upload

---
+++ inheritance/cube_of_digit.cpp
@@ -0,0 +1,40 @@
+//Q.1 Write a Program to find the sum of all three number’s cubes by implementing single-level inheritance: Class X->Class Y
+//- Class X has the following members: a, b & c attributes in integer data type
+//- Class Y has the following members: setData() and getData() methods
+#include<iostream>
+using namespace std;
+class x
+{
+	protected :
+		int a,b,c,ans;
+			
+			
+};
+class y : public x
+{
+	public :
+		void setData()
+		{
+		  cout << "Enter value of a : ";
+		  cin >> a;
+		  cout << "Enter value of b : ";
+		  cin >> b;
+		  cout << "Enter value of c : ";
+		  cin >> c;
+		  
+		}
+		void getData()
+		{
+			ans = a + b + c;
+			cout << endl <<"sum of digit : " << ans << endl ;
+			ans = ans*ans*ans;
+			cout << "cube is : " << ans;
+		}
+	
+};
+int main()
+{
+   	y t1;
+   	t1.setData();
+   	t1.getData();
+}
